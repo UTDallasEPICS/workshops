12b5508b71dd5933112910b208c4b959069ecc8f
TusharW4ni <reachtusharwani@gmail.com>
2025-08-02 20:53:28 -0500

    added deploy script and modified readme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,10 @@ export default defineNuxtConfig({
       EDITING_CODE: "",
     },
   },
+  app: {
+    baseURL: "/workshops/", // This is your repository name prefixed and suffixed with slashes
+    // Consider also setting buildAssetsDir if you face issues with asset paths later,
+    // though baseURL often handles it for static deployments.
+    // buildAssetsDir: '/_nuxt/', // Default, good to confirm
+  },
 });
